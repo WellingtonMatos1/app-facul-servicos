8ad634dec28ac5a12fa628b1ad54938cc310154d
João Brito <joaovitorb2345@gmail.com>
2024-04-20 14:32:16 -0300

    front

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $('#navbar').load('navbar.html');
+});
+
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, '\\$&');
+    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, ' '));
+}
--- main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-from routes import router as fornecedor_router
-from routes_servico import router as servico_router
-
-
-app = FastAPI()
-
-app.include_router(fornecedor_router)
-app.include_router(servico_router)
