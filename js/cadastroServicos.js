51f5f113ed7189d59cf70bcfc7980b5ba0ee5924
Wellington <Wellington3matos@hotmail.com>
2024-05-19 17:09:18 -0300

    botão para remover imagem

--- js/cadastroServicos.js
+++ js/cadastroServicos.js
@@ -10,3 +10,28 @@ document.getElementById('productImage').addEventListener('change', function() {
         reader.readAsDataURL(file);
     }
 });
+
+const productImage = document.getElementById('productImage');
+const imagePreview = document.querySelector('.image-preview');
+const removeButton = document.getElementById('removeButton');
+
+productImage.addEventListener('change', function() {
+    const file = this.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function() {
+            imagePreview.style.backgroundImage = `url('${reader.result}')`;
+            imagePreview.style.display = 'block';
+            removeButton.style.display = 'block';
+        }
+        reader.readAsDataURL(file);
+    }
+});
+
+removeButton.addEventListener('click', function() {
+    imagePreview.style.backgroundImage = 'none';
+    imagePreview.style.display = 'none';
+    productImage.value = '';
+    removeButton.style.display = 'none';
+});
+
