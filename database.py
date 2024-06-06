1e228c64097be018810210392de18ebe1a2837d8
Wellington <Wellington3matos@hotmail.com>
2024-06-05 22:02:51 -0300

    senha hash, token

--- database.py
+++ database.py
@@ -2,9 +2,12 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 from sqlalchemy import create_engine
 
 nome_banco = "trab"
-senha_postgres = "aqui"
+usuario_postgres = "postgres"
+senha_postgres = "senha_aqui"
+host_postgres = "localhost"
+porta_postgres = "5432"
 
-connection_string = f"postgresql://postgres:{senha_postgres}@localhost/{nome_banco}"
+connection_string = f"postgresql://{usuario_postgres}:{senha_postgres}@{host_postgres}:{porta_postgres}/{nome_banco}"
 
 engine = create_engine(connection_string, pool_pre_ping=True)
 
